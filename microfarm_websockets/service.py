f079331b7cd1b6c4675f5a1dea99007bcf31c521
Souheil Chelfouh <trollfot@gmail.com>
2023-03-23 12:14:14 +0100

    Added logging/example config

--- microfarm_websockets/service.py
+++ microfarm_websockets/service.py
@@ -1,8 +1,14 @@
+import jwt
 import asyncio
 import websockets
 import typing as t
+import json
+from pathlib import Path
 from aiozmq import rpc
 from minicli import run, cli
+
+
+logger = logging.getLogger('microfarm_websockets')
 
 
 class UserData(t.TypedDict):
@@ -12,8 +18,8 @@ class UserData(t.TypedDict):
 
 class WebsocketServer(rpc.AttrHandler):
 
-    def __init__(self, jwt_service: str = 'tcp://127.0.0.1:5555'):
-        self.jwt_service = jwt_service
+    def __init__(self, public_key: bytes):
+        self.public_key = public_key
         self.connections = {}
 
     @rpc.method
@@ -28,24 +34,54 @@ class WebsocketServer(rpc.AttrHandler):
         websockets.broadcast(self.connections.values(), message)
         return True
 
-    async def __call__(self, websocket):
-        jwt = await asyncio.wait_for(websocket.recv(), timeout=2)
-        service = await rpc.connect_rpc(connect=self.jwt_service)
-        userdata: UserData = await service.call.verify_token(jwt)
-
-        self.connections[userdata['email']] = websocket
+    def decode_jwt(self, token: str) -> dict:
         try:
-            await websocket.wait_closed()
-        finally:
-            del self.connections[userdata['email']]
+            return jwt.decode(token, self.public_key, algorithms=["RS256"])
+
+    async def __call__(self, websocket):
+        token = await asyncio.wait_for(websocket.recv(), timeout=2)
+        try:
+            userdata = jwt.decode(
+                token, self.public_key, algorithms=["RS256"])
+        except jwt.ExpiredSignatureError:
+            # specific error => token expired.
+            await websocket.send(json.dumps(
+                {"err": "Authentication failed."}
+            ))
+        except jwt.exceptions.InvalidTokenError:
+            await websocket.send(json.dumps(
+                {"err": "Authentication failed."}
+            ))
+        else:
+            self.connections[userdata['email']] = websocket
+            try:
+                await websocket.wait_closed()
+            finally:
+                del self.connections[userdata['email']]
 
 
 @cli
-async def serve():
-    service = WebsocketServer()
-    zeroserver = await rpc.serve_rpc(service, bind=f'tcp://127.0.0.1:6000')
-    async with websockets.serve(service, "", 6001):
-        await zeroserver.wait_closed()
+async def serve(config: Path, public_key: Path):
+    import tomli
+    import logging.config
+
+    assert config.is_file()
+    assert public_key.is_file()
+
+    with config.open("rb") as f:
+        settings = tomli.load(f)
+
+    with public_key.open("rb") as f:
+        public_key_pem = f.read()
+
+    if logconf := settings.get('logging'):
+        logging.config.dictConfigClass(logconf).configure()
+
+    service = WebsocketServer(public_key_pem)
+    server = await rpc.serve_rpc(service, bind=settings['rpc']['bind'])
+    async with websockets.serve(
+            service, settings['ws']['host'], settings['ws']['port']):
+        await server.wait_closed()
 
 
 if __name__ == '__main__':
